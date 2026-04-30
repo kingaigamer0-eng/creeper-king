b9227a700ed17b9eac074d774e7653286719a919
kingaigamer0-eng <kingaigamer0@gmail.com>
2026-04-30 22:24:53 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made by creeper king",
         // Activity type number (0 = Playing).
         type: 0, 
       },
